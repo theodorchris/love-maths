8eea0a524ae0b99d58b905ed697ae8369e198ae3
Theo <theodor.chris@gmail.com>
2022-10-27 18:27:37 +0000

    Add code to calculate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,32 +10,22 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                runGame(gameType);
+                alert(`You clicked ${gameType}`);
             }
         });
     }
-
-    runGame("addition");
-
 });
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(gameType) {
+
+function runGame() {
 
     // Creates two random numbers between 1 and 25
-    let num1 = Math.floor(Math.random() * 25) + 1;
-    let num2 = Math.floor(Math.random() * 25) + 1;
-
-    if (gameType === "addition") {
-        displayAdditionQuestion(num1, num2);
-    } else {
-        alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. Aborting!`;
-    }
-
+    let num1 = Math.floor (Math.random() * 25) + 1
+    let num2 = Math.floor (Math.random() * 25) + 1
 }
 
 function checkAnswer() {
@@ -54,12 +44,8 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion(operand1, operand2) {
+function displayAdditionQuestion() {
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "+";
-    
 }
 
 function displaySubtractQuestion() {
